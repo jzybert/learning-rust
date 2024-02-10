b92ffc0a43745be41625eafa5d3e5a8395367f95
jzybert <jmzybert@gmail.com>
2024-02-10 09:55:01 -0800

    Plus one

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,5 +4,16 @@ fn main() {
         x
     };
 
-    println!("{y}")
+    println!("{y}");
+
+    let x = plus_one(five());
+    println!("Six: {x}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
